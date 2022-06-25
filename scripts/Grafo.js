0e226387667875dd70f278e176c3029179032650
Carlos Sandoval <csandovalc@unal.edu.co>
2022-06-25 00:10:03 -0500

    Finalizacion de la graficacion

--- scripts/Grafo.js
+++ scripts/Grafo.js
@@ -3,6 +3,7 @@ class Grafo {
 		this.numVertices = numVertices;
 		this.nodos = [];
 		this.conexiones = {};
+		this.matrizAdyacencia = [];
 	}
 
 	// Metodo para agregar un nodo al grafo
@@ -27,6 +28,14 @@ class Grafo {
 
 	// Metodo para agregar una arista entre dos nodos
 	agregarArista(nodo1, nodo2) {
+		// No tener en cuenta conexiones repetidas
+		if (
+			Object.keys(this.conexiones).includes(`${nodo1.id}-${nodo2.id}`) ||
+			Object.keys(this.conexiones).includes(`${nodo2.id}-${nodo1.id}`)
+		) {
+			return;
+		}
+
 		let xInicial = nodo1.x,
 			yInicial = nodo1.y;
 
@@ -39,6 +48,8 @@ class Grafo {
 			xFinal,
 			yFinal,
 		});
+
+		return this.conexiones[`${nodo1.id}-${nodo2.id}`];
 	}
 
 	// Metodo para eliminar una arista entre dos nodos
@@ -54,5 +65,26 @@ class Grafo {
 		for (let i = 0; i < this.nodos.length; i++) {
 			this.nodos[i].dibujar();
 		}
+	}
+
+	crearMatrizAdyacencia() {
+		// Creamos la matriz de adyacencia
+		for (let i = 0; i < this.numVertices; i++) {
+			let temp = [];
+			for (let j = 0; j < this.numVertices; j++) {
+				i === j ? temp.push(0) : temp.push(0);
+			}
+			this.matrizAdyacencia.push(temp);
+		}
+
+		for (let conexion in this.conexiones) {
+			let indicesNodos = conexion.split("-").map((n) => parseInt(n));
+			this.matrizAdyacencia[indicesNodos[0]][indicesNodos[1]] = 1;
+			this.matrizAdyacencia[indicesNodos[1]][indicesNodos[0]] = 1;
+		}
+
+		console.info("MATRIZ ADYACENCIA: ");
+		console.log(this.matrizAdyacencia);
+		console.log(JSON.stringify(this.matrizAdyacencia));
 	}
 }
--- scripts/Grafo_Aleatorio.js
+++ scripts/Grafo_Aleatorio.js
@@ -1,4 +1,6 @@
 class Grafo_Aleatorio extends Grafo {
+	static PROBABILIDAD_ARISTA = 0.5;
+
 	constructor(numVertices, ancho, alto) {
 		super(numVertices);
 		this.ancho = ancho - Nodo.TAMANO_NODO;
@@ -28,10 +30,48 @@ class Grafo_Aleatorio extends Grafo {
 		return this.agregarNodo(x, y);
 	}
 
-	generar() {
+	generarAristas(plano = true) {
+		let nodo1 = random(this.nodos);
+		let nodo2 = random(this.nodos);
+
+		if (nodo1 === nodo2) {
+			return;
+		}
+
+		if (plano) {
+			let colision = false;
+			for (let conexion in this.conexiones) {
+				colision = collideLineLine(
+					nodo1.x,
+					nodo1.y,
+					nodo2.x,
+					nodo2.y,
+					this.conexiones[conexion].posInicial.x,
+					this.conexiones[conexion].posInicial.y,
+					this.conexiones[conexion].posFinal.x,
+					this.conexiones[conexion].posFinal.y
+				);
+				if (colision) {
+					return this.generarAristas();
+				}
+			}
+		}
+		return this.agregarArista(nodo1, nodo2);
+	}
+
+	generar(plano = true) {
 		for (let i = 0; i < this.numVertices; i++) {
 			this.generarNodo();
 		}
+		for (let i = 0; i < this.numVertices; i++) {
+			for (let j = i + 1; j < this.numVertices; j++) {
+				if (random() < Grafo_Aleatorio.PROBABILIDAD_ARISTA) {
+					this.generarAristas(plano);
+				}
+			}
+		}
+
+		this.crearMatrizAdyacencia();
 	}
 
 	dibujar() {
